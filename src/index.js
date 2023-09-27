1a5fc5b4f394dd687b0533e7199d19fc46b46d8f
SplitCode <natacsp@mail.ru>
2023-09-27 22:59:08 +0300

    feat: add function genDiff

--- src/index.js
+++ src/index.js
@@ -11,31 +11,26 @@ const genDiff = (filepath1, filepath2) => {
   const file1 = JSON.parse(fs.readFileSync(absolutePath1, 'utf8'));
   const file2 = JSON.parse(fs.readFileSync(absolutePath2, 'utf8'));
 
-  console.log('File 1 content:', file1);
-  console.log('File 2 content:', file2);
+  // console.log('File 1 content:', file1);
+  // console.log('File 2 content:', file2);
 
-//   const keys1 = Object.keys(file1);
-//   const keys2 = Object.keys(file2);
-//   const keys = _.union(keys1, keys2);
+  const keys1 = Object.keys(file1);
+  const keys2 = Object.keys(file2);
+  const keys = _.union(keys1, keys2);
+  const sortedKeys = _.sortBy(keys);
+  // console.log(sortedKeys);
 
-//   const result = {};
-//   for (const key of keys) {
-//     if (!Object.hasOwn(data1, key)) {
-//       result[key] = 'added';
-//     } else if (!Object.hasOwn(data2, key)) {
-//         result[key] = 'deleted';
-//       } else if (data1[key] !== data2[key]) {
-//         result[key] = 'changed';
-//       } else {
-//         result[key] = 'unchanged';
-//       }
-//     }
-
-//     return result;
+  const diff = sortedKeys.map((key) => {
+    if (!Object.hasOwn(file1, key)) {
+      return `  + ${key}: ${file2[key]}`;
+    } if (!Object.hasOwn(file2, key)) {
+      return `  - ${key}: ${file1[key]}`;
+    } if (file1[key] !== file2[key]) {
+      return `  - ${key}: ${file1[key]}\n  + ${key}: ${file2[key]}`;
+    }
+    return `    ${key}: ${file1[key]}`;
+  });
+  return ['{', ...diff, '}'].join('\n');
 };
 
 export default genDiff;
-
-// файлы парсим и обрабатываем через функции readFileSync, path, библиотеки
-
-// (filepath) => path.resolve(process.cwd(), filepath);
