67dc755cf106bafd61013253d99bc428f1c19c7b
SplitCode <natacsp@mail.ru>
2023-10-02 15:56:35 +0300

    fix: fix test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -10,8 +10,8 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-const firstFile = getFixturePath('__fixtures__/file1.json');
-const secondFile = getFixturePath('__fixtures__/file2.json');
+const firstFile = getFixturePath('file1.json');
+const secondFile = getFixturePath('file2.json');
 
 // const result = fs.readFileSync(getFixturePath('expected.txt'), 'utf-8')
 
